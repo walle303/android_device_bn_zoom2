5c3d382140e322366c42ea27cd7c45ba77c2daaa
kfazz <kfazz01@gmail.com>
2015-04-24 11:13:39 -0400

    add cfg.bin to intiial sdcard so uboot images show up.
    Change-Id: I6407b1979294579451e87f27a3a342ed63554c3d

--- create_sd.sh
+++ create_sd.sh
@@ -34,9 +34,10 @@ cp "2ndbootloader" "./tmpmount/u-boot.bin"
 cp "kernel" "./tmpmount/uImage-recovery"
 cp "ramdisk-recovery.ub" "./tmpmount/ramdisk-recovery.ub"
 
-#copy dsp firmware, waveform, and stock images used by u-boot.
+#copy dsp firmware, waveform, cfg, and stock images used by u-boot.
 cp "../../../../device/bn/zoom2/prebuilt/boot/flash_spl.bin" "./tmpmount/"
 cp "../../../../device/bn/zoom2/prebuilt/boot/wvf.bin" "./tmpmount/"
+cp "../../../../device/bn/zoom2/prebuilt/boot/cfg.bin" "./tmpmount/"
 cp "../../../../device/bn/zoom2/prebuilt/boot/dead.pgm" "./tmpmount/"
 cp "../../../../device/bn/zoom2/prebuilt/boot/booting.pgm" "./tmpmount/"
 cp "../../../../device/bn/zoom2/prebuilt/boot/charging0.pgm" "./tmpmount/"
